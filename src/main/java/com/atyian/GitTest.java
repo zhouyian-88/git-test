7f48906da867107b310ccfd779f27c7fabc508e0
yianzhou <2873642764@qq.com>
2022-08-14 16:34:25 +0800

    first commit

---
+++ src/main/java/com/atyian/GitTest.java
@@ -0,0 +1,13 @@
+package com.atyian;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-08-14-16:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
